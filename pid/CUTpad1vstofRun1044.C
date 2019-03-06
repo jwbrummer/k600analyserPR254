2c066b22bd28b78b3cd1e4bde9eac6ec0f637ef8
jwbrummer <jbrummer@sun.ac.za>
2019-03-06 16:29:44 +0200

    Adding the PID macros for the triton-tree sorter code

---
+++ pid/CUTpad1vstofRun1044.C
@@ -0,0 +1,25 @@
+{
+//========= Macro generated from object: CUTpad1vstofRun1044/Graph
+//========= by ROOT version5.34/39
+   
+   TCutG *cutg = new TCutG("CUTpad1vstofRun1044",14);
+   cutg->SetVarX("tof");
+   cutg->SetVarY("pad1");
+   cutg->SetTitle("Graph");
+   cutg->SetFillColor(1);
+   cutg->SetPoint(0,3495.92,1859.18);
+   cutg->SetPoint(1,3534.6,1815.66);
+   cutg->SetPoint(2,3560.39,1633.7);
+   cutg->SetPoint(3,3604.71,1321.2);
+   cutg->SetPoint(4,3627.27,1071.99);
+   cutg->SetPoint(5,3615.19,862.342);
+   cutg->SetPoint(6,3596.65,806.962);
+   cutg->SetPoint(7,3562,858.386);
+   cutg->SetPoint(8,3535.4,949.367);
+   cutg->SetPoint(9,3498.33,1166.93);
+   cutg->SetPoint(10,3458.04,1487.34);
+   cutg->SetPoint(11,3456.43,1637.66);
+   cutg->SetPoint(12,3466.1,1744.46);
+   cutg->SetPoint(13,3495.92,1859.18);
+   cutg->Draw("");
+}
